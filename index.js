8da807903dfb8c9e1941322a0980494edfab856f
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2020-03-23 23:08:43 +0300

    nextjs app

--- index.js
+++ index.js
@@ -6,74 +6,76 @@ const { AdminUIApp } = require('@keystonejs/app-admin-ui');
 const initialiseData = require('./initial-data');
 
 const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const { NextApp } = require('@keystonejs/app-next');
 
 const PROJECT_NAME = "covid-supply-info";
 
 
 const keystone = new Keystone({
-  name: PROJECT_NAME,
-  adapter: new Adapter(),
-  onConnect: initialiseData,
+    name: PROJECT_NAME,
+    adapter: new Adapter(),
+    onConnect: initialiseData,
 });
 
 // Access control functions
 const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
 const userOwnsItem = ({ authentication: { item: user } }) => {
-  if (!user) {
-    return false;
-  }
-  return { id: user.id };
+    if (!user) {
+        return false;
+    }
+    return { id: user.id };
 };
 
 const userIsAdminOrOwner = auth => {
-  const isAdmin = access.userIsAdmin(auth);
-  const isOwner = access.userOwnsItem(auth);
-  return isAdmin ? isAdmin : isOwner;
+    const isAdmin = access.userIsAdmin(auth);
+    const isOwner = access.userOwnsItem(auth);
+    return isAdmin ? isAdmin : isOwner;
 };
 
 const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
 
 keystone.createList('User', {
-  fields: {
-    name: { type: Text },
-    email: {
-      type: Text,
-      isUnique: true,
+    fields: {
+        name: { type: Text },
+        email: {
+            type: Text,
+            isUnique: true,
+        },
+        isAdmin: {
+            type: Checkbox,
+            // Field-level access controls
+            // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
+            access: {
+                update: access.userIsAdmin,
+            },
+        },
+        password: {
+            type: Password,
+        },
     },
-    isAdmin: {
-      type: Checkbox,
-      // Field-level access controls
-      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
-      access: {
-        update: access.userIsAdmin,
-      },
+    // List-level access controls
+    access: {
+        read: access.userIsAdminOrOwner,
+        update: access.userIsAdminOrOwner,
+        create: access.userIsAdmin,
+        delete: access.userIsAdmin,
+        auth: true,
     },
-    password: {
-      type: Password,
-    },
-  },
-  // List-level access controls
-  access: {
-    read: access.userIsAdminOrOwner,
-    update: access.userIsAdminOrOwner,
-    create: access.userIsAdmin,
-    delete: access.userIsAdmin,
-    auth: true,
-  },
 });
 
 const authStrategy = keystone.createAuthStrategy({
-  type: PasswordAuthStrategy,
-  list: 'User',
+    type: PasswordAuthStrategy,
+    list: 'User',
 });
 
 module.exports = {
-  keystone,
-  apps: [
-    new GraphQLApp(),
-    new AdminUIApp({
-      enableDefaultRoute: true,
-      authStrategy,
-    }),
-  ],
+    keystone,
+    apps: [
+        new GraphQLApp(),
+        new AdminUIApp({
+            enableDefaultRoute: false,
+            authStrategy,
+        }),
+        new NextApp({ dir: 'app' }),
+    ],
 };
