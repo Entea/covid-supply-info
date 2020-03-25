f08e76a679d0580bcbbd6f62c6657392d77e4b71
Isabek Tashiev <isabek2309@gmail.com>
2020-03-25 19:55:31 +0200

    #5 Add Need and Need type for hospitals. (#24)

--- backend/distributor/admin.py
+++ backend/distributor/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from rangefilter.filter import DateRangeFilter
 
-# Register your models here.
+from distributor.models import Need, NeedType
+
+
+@admin.register(Need)
+class NeedAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name',
+    )
+
+    list_display = (
+        '__str__',
+        'type',
+        'created_at',
+        'modified_at',
+        'created_by',
+        'modified_by',
+    )
+
+    list_filter = (
+        ('created_at', DateRangeFilter),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'created_by', None) is None:
+            obj.created_by = request.user
+        obj.modified_by = request.user
+        obj.save()
+
+
+@admin.register(NeedType)
+class NeedTypeAdmin(admin.ModelAdmin):
+    pass
