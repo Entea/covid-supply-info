c66c0c5edb85e70fa682c0c816fa13d0efdb9bec
Isabek Tashiev <isabek2309@gmail.com>
2020-03-25 21:27:28 +0200

    #6 Add donation model.

--- backend/distributor/admin.py
+++ backend/distributor/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from rangefilter.filter import DateRangeFilter
 
-from distributor.models import Measure, NeedType
+from distributor.models import Measure, NeedType, Donation, DonationDetail
 
 
 @admin.register(NeedType)
@@ -29,7 +29,46 @@ class NeedAdmin(admin.ModelAdmin):
         obj.modified_by = request.user
         obj.save()
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 @admin.register(Measure)
 class NeedTypeAdmin(admin.ModelAdmin):
     pass
+
+
+class DonationDetailInline(admin.TabularInline):
+    model = DonationDetail
+
+    def has_module_permission(self, request):
+        return False
+
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    inlines = (DonationDetailInline,)
+
+    search_fields = (
+        'donator_name',
+    )
+
+    list_display = (
+        '__str__',
+        'donator_type',
+        'created_at',
+        'modified_at',
+        'created_by',
+        'modified_by',
+    )
+
+    list_filter = (
+        'donator_type',
+        ('created_at', DateRangeFilter),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'created_by', None) is None:
+            obj.created_by = request.user
+        obj.modified_by = request.user
+        obj.save()
--- backend/distributor/models.py
+++ backend/distributor/models.py
@@ -27,3 +27,37 @@ class NeedType(models.Model):
 
     def __str__(self):
         return "%s %s" % (self.name, self.measure)
+
+
+class Donation(models.Model):
+    ORGANIZATION = 'ORGANIZATION'
+    PERSON = 'PERSONAL'
+
+    DONATOR_TYPES = (
+        (ORGANIZATION, _('ORGANIZATION')),
+        (PERSON, _('PERSONAL'))
+    )
+    donator_type = models.CharField(verbose_name=_('Donator Type'), choices=DONATOR_TYPES, max_length=12,
+                                    default=ORGANIZATION)
+    donator_name = models.CharField(max_length=200, verbose_name=_('Donator Name'), null=False, blank=False)
+    description = models.TextField(verbose_name=_("Donation Description"), max_length=1000, blank=False)
+
+    modified_at = models.DateTimeField(verbose_name=_('Modified Date'), auto_now=True, null=True, blank=True,
+                                       editable=False)
+    created_at = models.DateTimeField(verbose_name=_('Created Date'), blank=False, null=False, editable=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='%(class)s_created_by',
+                                   auto_created=True, editable=False)
+    modified_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='%(class)s_modified_by',
+                                    auto_created=True, editable=False)
+
+    def __str__(self):
+        return self.donator_name
+
+
+class DonationDetail(models.Model):
+    need_type = models.ForeignKey(NeedType, on_delete=models.PROTECT, verbose_name=_('Need Type'))
+    amount = models.PositiveSmallIntegerField(verbose_name=_('Amount'))
+    donation = models.ForeignKey(Donation, on_delete=models.PROTECT, verbose_name=_('Donation)'), null=False)
+
+    def __str__(self):
+        return "{} {}".format(self.need_type, self.amount)
