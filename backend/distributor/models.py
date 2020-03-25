0c773a2e441d54edcc3744cd88319ac523ecbc74
Isabek Tashiev <isabek2309@gmail.com>
2020-03-25 20:27:49 +0200

    #5 Rename Need to NeedType and NeedType to Measure. (#25)

--- backend/distributor/models.py
+++ backend/distributor/models.py
@@ -4,16 +4,17 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 
-class NeedType(models.Model):
-    name = models.CharField(max_length=200, verbose_name=_('Name'), unique=True)
+class Measure(models.Model):
+    name = models.CharField(max_length=200, verbose_name=_('Name (liters, kg, etc.)'), unique=True)
 
     def __str__(self):
         return self.name
 
 
-class Need(models.Model):
+class NeedType(models.Model):
     name = models.CharField(max_length=200, verbose_name=_('Name'), null=False, blank=False)
-    type = models.ForeignKey(NeedType, on_delete=models.PROTECT, verbose_name=_('Type'), null=False)
+    measure = models.ForeignKey(Measure, on_delete=models.PROTECT, verbose_name=_('Measure (liters, kg, etc.)'),
+                                null=True)
     price_per_piece = models.DecimalField(max_digits=12, decimal_places=2, verbose_name=_("Price per piece (KGS)"),
                                           null=False)
     modified_at = models.DateTimeField(verbose_name=_('Modified Date'), auto_now=True, null=True, blank=True,
@@ -25,4 +26,4 @@ class Need(models.Model):
                                     auto_created=True, editable=False)
 
     def __str__(self):
-        return "%s %s" % (self.name, self.type)
+        return "%s %s" % (self.name, self.measure)
