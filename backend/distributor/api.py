51cf79185e60b332dbed53c7cf0a949acf3a7509
Isabek Tashiev <isabek2309@gmail.com>
2020-03-27 23:50:24 +0200

    #35 Remove unused ListAPIView.

--- backend/distributor/api.py
+++ backend/distributor/api.py
@@ -1,14 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
-from rest_framework.generics import ListAPIView
 
 from distributor.models import Hospital, Donation, Region, District, Locality
 from distributor.serializers import HospitalSerializer, DonationSerializer, RegionSerializer, DistrictSerializer, \
     LocalitySerializer
 
 
-class HospitalViewSet(viewsets.ModelViewSet, ListAPIView):
+class HospitalViewSet(viewsets.ModelViewSet):
     """
     API returns the list of the hospitals
     """
