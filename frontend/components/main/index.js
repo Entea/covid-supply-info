06a6f3a66c9aa0530a88fd81bcd59ad66bcd77e6
Isabek Tashiev <isabek2309@gmail.com>
2020-03-30 19:50:14 +0300

    #85 Add filter by region, district, locality. (#99)
    * #85 Add filter by region, district, locality.
    
    * Update frontend/reducers/hospitalsReducer.js
    
    * Resolve change requests.
    
    * filter and search refactor
    
    * Small improvement.
    
    Co-authored-by: Elzar Esen <elzaresen@outlook.com>

--- frontend/components/main/index.js
+++ frontend/components/main/index.js
@@ -1,63 +1,54 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import getConfig from 'next/config';
+import { connect } from 'react-redux';
+
 const { publicRuntimeConfig } = getConfig();
 
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-
-import {
-  fetchHospitals as fetchHospitalsAction,
-} from '../../actions/creators/hospitals'
-
-
 class Main extends Component {
-
-  componentDidMount() {
-    this.props.fetchHospitalsAction();
-  }
+	static defaultProps = {
+		center: {
+			lat: 41.822475,
+			lng: 74.754128
+		},
+		zoom: 7,
+	};
 
 	render() {
-    const { fetching, hospitals } = this.props;
+		const Mark = ({ name }) => <div className="circle-mark">
+			<div className="circle"/>
+			<div className="name">{name}</div>
+		</div>;
 
-    const Mark = ({name}) => <div className="circle-mark"><div className="circle"/><div className="name">{name}</div></div>;
-    let Marks = [];
-    hospitals.forEach((item, index) => {
-      if (item.full_location.latitude && item.full_location.longitude) {
-        Marks.push(<Mark
-          key={index + 'm'}
-          lat={ item.full_location.longitude }
-          lng={ item.full_location.latitude }
-          name={item.name}
-        />)
-      }
+		let marks = (this.props.hospitals || [])
+			.filter(item => item['full_location'].lat && item['full_location'].lng)
+			.map((item, index) => (<Mark
+				key={index + 'm'}
+				lat={item['full_location'].lat}
+				lng={item['full_location'].lng}
+				name={item.name}
+			/>));
 
-    })
 		return (
 			<main>
-				<div style={ { height: '100vh', width: '100%' } }>
-          {Marks.length !==0 ? <GoogleMapReact
-						bootstrapURLKeys={ { key: publicRuntimeConfig.mapKey } }
-						defaultCenter={ { lat: 41.822475, lng: 74.754128 } }
-						defaultZoom={ 7 }
-					>
-            {Marks.map((item) => item)}
-					</GoogleMapReact> : null}
+				<div style={{ height: '100vh', width: '100%' }}>
+					<GoogleMapReact
+						bootstrapURLKeys={{ key: publicRuntimeConfig.mapKey }}
+						defaultCenter={this.props.center}
+						defaultZoom={this.props.zoom}>
+						{marks}
+					</GoogleMapReact>
 				</div>
 			</main>
 		);
-  }
-};
+	}
+}
 
 const mapStateToProps = (state) => {
-  return {
-    fetching: state.hospitals.fetching,
-    hospitals: state.hospitals.results
-  }
+	return {
+		fetching: state.hospitals.fetching,
+		hospitals: state.hospitals.results
+	}
 };
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({
-  fetchHospitalsAction,
-}, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(Main)
+export default connect(mapStateToProps, null)(Main)
