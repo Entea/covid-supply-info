a9e2edb21a2882ca67db5cf98f0e24e210fd149d
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2020-03-31 00:21:43 +0300

    fix contact component name and indents

--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -2,22 +2,22 @@ import React, {Component} from 'react';
 import Layout from '../components/layout/Layout';
 import { Col, Container, Row } from 'react-bootstrap'
 
-class Home extends Component {
+class Contact extends Component {
     render() {
         return (
             <Layout>
-			<main>
-				<Container>
-					<Row style={{ marginTop: 150 }}>
-						<Col xs={12}>
-                            Page is not ready yet
-                        </Col>
-                    </Row>
-                </Container>
-            </main>
+                <main>
+                    <Container>
+                        <Row style={{ marginTop: 150 }}>
+                            <Col xs={12}>
+                                Page is not ready yet
+                            </Col>
+                        </Row>
+                    </Container>
+                </main>
             </Layout>
         );
     }
 }
 
-export default Home
+export default Contact
