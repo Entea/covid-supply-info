a7653ba431162a9b468ad270471cccc59bd49b42
joogazyn <joogazyn864@gmail.com>
2020-03-31 13:09:53 +0600

    Task #43

--- frontend/components/main/index.js
+++ frontend/components/main/index.js
@@ -8,19 +8,151 @@ import { bindActionCreators } from 'redux'
 
 import {
   fetchHospitals as fetchHospitalsAction,
+  fetchHospital as fetchHospitalAction,
 } from '../../actions/creators/hospitals'
 
 
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      openRightBlock: true,
+      rightBlockTemplate: null
+    }
+    this.closeRightBlock.bind(this);
+    this.getHospitalsInfo.bind(this);
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
 
   componentDidMount() {
-    this.props.fetchHospitalsAction();
+    this.props.fetchHospitalsAction().then(() => {
+      this.openHospitals()
+    });
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target) && this.state.openRightBlock && event.target.className.indexOf('circle') === -1) {
+      this.closeRightBlock();
+    }
+  }
+
+  needHelpStatus(percent) {
+    switch (percent) {
+      case (39 < percent && percent > 0):
+        return 'danger';
+      case (85 < percent && percent > 39):
+        return 'warning';
+      default:
+        return '';
+    }
+  }
+
+  closeRightBlock() {
+    this.setState({openRightBlock: false, rightBlockTemplate: null});
+  }
+
+  openHospital(m, props) {
+    this.props.fetchHospitalAction(props.id).then(() => {
+      this.state.rightBlockTemplate = this.getHospitalInfo();
+      this.setState({openRightBlock: true})
+    });
+  }
+
+  openHospitals() {
+    this.state.rightBlockTemplate = this.getHospitalsInfo();
+    this.setState({openRightBlock: true})
+  }
+
+  getHospitalsInfo() {
+    const {hospitals} = this.props;
+    return (<table key={hospitals} className="hospital-list">
+              <thead>
+              <tr>
+                <th className="title">Больница</th>
+                <th className="title">Уровень помощи</th>
+              </tr>
+              </thead>
+              <tbody>
+              {hospitals.map((item, index) =>
+                (<tr key={index}>
+                  <td>
+                    {item.name}
+                  </td>
+                  <td>
+                    <div className={'circle ' + this.needHelpStatus(item.request_amount)}/>
+                    <div className="percentage">
+                      {item.request_amount ? item.request_amount : 0}%
+                    </div>
+                  </td>
+                </tr>))}
+              </tbody>
+            </table>)
+  }
+
+  getHospitalInfo() {
+    const {hospital} = this.props;
+    return (<div className="hospital-box">
+      <h1>{hospital.name}</h1>
+      <div className="address">
+        {hospital.address}
+        <div className="coordinate">{hospital.full_location ?  hospital.full_location.longitude + ', ' +  hospital.full_location.latitude : ''}</div>
+      </div>
+      <ul className="phone">
+        {hospital.phone_numbers.map((item) => <li>{item}</li>)}
+      </ul>
+      <h1>Статистика по мед.учреждению</h1>
+      {hospital.statistics.length === 0 ? <p>Нет данных</p> : null}
+      {hospital.statistics.map((item, index) => (
+        <div key={index + 's'} className="circle">
+          <div className="name">{item.category}</div>
+          <div className="count">{item.actual}/{item.capacity}</div>
+        </div>
+      ))}
+      <h1>Продукция по спецодежде и оборудованию в больнице…</h1>
+      <table>
+        <thead>
+        <tr>
+          <th>Название</th>
+          <th>Наличие</th>
+          <th>Потребности</th>
+          <th>Не хватает</th>
+        </tr>
+        </thead>
+        <tbody>
+        {hospital.needs.map((item, index) => (
+          <tr key={index + 'n'}>
+            <td>
+              {index + 1}.{item.need_type ? item.need_type.name : ''}
+            </td>
+            <td>
+              {item.need_type ? item.need_type.reserve_amount : 0}
+            </td>
+            <td>
+              {item.need_type ? item.need_type.request_amount : 0}
+            </td>
+            <td>
+              {item.need_type ? item.need_type.request_amount - item.need_type.reserve_amount : 0}
+            </td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>)
   }
 
 	render() {
-    const { fetching, hospitals } = this.props;
-
-    const Mark = ({name}) => <div className="circle-mark"><div className="circle"/><div className="name">{name}</div></div>;
+    const { hospitals } = this.props;
+    const Mark = ({name, percent}) => <div className="circle-mark"><div className={'circle ' + this.needHelpStatus(percent)}/><div className="name">{name}</div></div>;
     let Marks = [];
     hospitals.forEach((item, index) => {
       if (item.full_location.latitude && item.full_location.longitude) {
@@ -29,6 +161,8 @@ class Main extends Component {
           lat={ item.full_location.longitude }
           lng={ item.full_location.latitude }
           name={item.name}
+          id={item.id}
+          percent={item.request_amount}
         />)
       }
 
@@ -36,13 +170,21 @@ class Main extends Component {
 		return (
 			<main>
 				<div style={ { height: '100vh', width: '100%' } }>
-          {Marks.length !==0 ? <GoogleMapReact
+          <GoogleMapReact
 						bootstrapURLKeys={ { key: publicRuntimeConfig.mapKey } }
 						defaultCenter={ { lat: 41.822475, lng: 74.754128 } }
 						defaultZoom={ 7 }
+            onChildClick={this.openHospital.bind(this)}
 					>
-            {Marks.map((item) => item)}
-					</GoogleMapReact> : null}
+            {Marks}
+					</GoogleMapReact>
+          {this.state.openRightBlock }
+          <div ref={this.setWrapperRef} className={ this.state.openRightBlock ? 'open right-block' : 'right-block'}>
+            <a className="close" onClick={() => this.closeRightBlock()}>
+              <img src="x.svg" alt="close"/>
+            </a>
+            {this.state.rightBlockTemplate}
+          </div>
 				</div>
 			</main>
 		);
@@ -52,12 +194,14 @@ class Main extends Component {
 const mapStateToProps = (state) => {
   return {
     fetching: state.hospitals.fetching,
-    hospitals: state.hospitals.results
+    hospitals: state.hospitals.results,
+    hospital: state.hospitals.single
   }
 };
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({
   fetchHospitalsAction,
+  fetchHospitalAction,
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main)
