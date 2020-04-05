ac2a18c5e3d620fd3e213586f390caeb747af868
Aibek Raiymbekov <aibek.raiymbekov01@gmail.com>
2020-04-06 00:17:43 +0600

    wrapped statuses text

--- backend/distributor/constants.py
+++ backend/distributor/constants.py
@@ -1,9 +1,11 @@
+from django.utils.translation import ugettext as _
+
 READY_TO_SEND = 'ready_to_sent'
 SENT = 'sent'
 DELIVERED = 'delivered'
 
 DISTRIBUTION_STATUSES = (
-    (READY_TO_SEND, 'Подготовлено'),
-    (SENT, 'Отправлено'),
-    (DELIVERED, 'Доставлено'),
+    (READY_TO_SEND, _('Подготовлено')),
+    (SENT, _('Отправлено')),
+    (DELIVERED, _('Доставлено')),
 )
