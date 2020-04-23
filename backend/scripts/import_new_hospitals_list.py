a3bb7e0c65cd3819d5c47fda21ae3f9e977e75b9
Nurlan Turdaliev <nurlan0000@gmail.com>
2020-04-23 12:00:29 +0600

    Review fixes

--- backend/scripts/import_new_hospitals_list.py
+++ backend/scripts/import_new_hospitals_list.py
@@ -2,6 +2,7 @@ from distributor.models import Region, District, Locality, Hospital
 import csv, requests, os
 from django.contrib.gis.geos import fromstr
 
+# Script imports data that wasn't imported by import_hospitals.py
 def run():
     with open('scripts/new_hospital_list.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -11,18 +12,14 @@ def run():
                 print(f'Column names are {", ".join(row)}')
             else:
                 region_name = row[0]
-                try:
-                    region = Region.objects.get(name=region_name)
-                except:
-                    region = Region(name=region_name)
-                    region.save()
+                region, created = Region.objects.get_or_create(name=region_name)
+                if created:
+                    print(f'\tCreated region: {region_name}')
 
                 district_name = row[1]
-                try:
-                    district = District.objects.get(name=district_name, region=region)
-                except:
-                    district = District(name=district_name, region=region)
-                    district.save()
+                district, created = District.objects.get_or_create(name=district_name, region=region)
+                if created:
+                    print(f'\tCreated district: {district_name}')
 
                 hospital_code = row[2]
                 hospital_name = row[4]
@@ -40,13 +37,17 @@ def run():
                                 locality = l
                                 break
                     if locality is None:
-                        locality = Locality(name=district_name, district=district)
-                        locality.save()
+                        locality, created = Locality.objects.get_or_create(name=district_name, district=district)
+                        if created:
+                            print(f'Creating new locality: {district_name}')
+
+                    # Create new hospital and fill out fields
                     hospital = Hospital(code=hospital_code, name=hospital_name, locality=locality)
 
                 r = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json', 
                     params={'input': f'{address}', 'inputtype':'textquery', 'fields': 'name,geometry', 'key': os.environ.get("MAP_API_KEY")})
                 candidates = r.json()["candidates"]
+                print(f'Candidates: {candidates}')
                 candidate = None
                 if len(candidates) > 0:
                     candidate = candidates[0]
@@ -54,8 +55,10 @@ def run():
                 if candidate:
                     lat = candidate["geometry"]["location"]["lat"]
                     lng = candidate["geometry"]["location"]["lng"]
-                    hospital.location = fromstr(f'POINT({lng} {lat})', srid=4326)
-                    
+                    new_loc = fromstr(f'POINT({lng} {lat})', srid=4326)
+                    print(f'Updating hospital location from {hospital.location} to {new_loc}')
+                    hospital.location = new_loc
+
                 hospital.save()
                 
                 print(f'{region.name} \t{district.name} \t{locality.name} \t {hospital.name}.')
