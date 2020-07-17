50f531a41c7927659365fd145bc626a0335f871c
Aibek Raiymbekov <aibek.raiymbekov01@gmail.com>
2020-07-18 01:07:15 +0600

    Logout implementation (#197)
    * hospital needs implementation
    
    * import fix
    
    * urls changed
    
    * logout view
    
    * logout view
    
    * resolved comments
    
    * hospital needs implementation
    
    * import fix
    
    * urls changed
    
    * logout view
    
    * logout view
    
    * 185 hospitals list (#195)
    
    * Home page with hospitals list markup
    
    * get method in homepage
    
    * route change after login
    
    * hospitals iterator
    
    * data type optimisation
    
    * removed add button in home page, token service in main implemented as singletone
    
    * resolved comments
    
    * hospital needs implementation
    
    * import fix
    
    * urls changed
    
    * logout view
    
    * logout view
    
    * 185 hospitals list (#195)
    
    * Home page with hospitals list markup
    
    * get method in homepage
    
    * route change after login
    
    * hospitals iterator
    
    * data type optimisation
    
    * removed add button in home page, token service in main implemented as singletone
    
    * resolved comments
    
    * hospital needs implementation
    
    * urls changed
    
    * logout view
    
    * resolved comments
    
    * updated message
    
    * updated serializer
    
    Co-authored-by: Elzar Esen <elzaresen@outlook.com>

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,22 @@
+from django.contrib.auth import get_user_model
 from rest_auth.models import TokenModel
 from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserShortSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('id', 'full_name')
+
+    def get_full_name(self, user):
+        return "{first_name} {last_name}".format(
+            first_name=user.first_name,
+            last_name=user.last_name
+        )
 
 
 class TokenSerializer(serializers.ModelSerializer):
@@ -9,6 +26,8 @@ class TokenSerializer(serializers.ModelSerializer):
 
     token = serializers.CharField(source='key')
 
+    user = UserShortSerializer()
+
     class Meta:
         model = TokenModel
-        fields = ('token',)
+        fields = ('token', 'user')
