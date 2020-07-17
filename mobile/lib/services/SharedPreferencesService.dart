cc526efe4ce78461ead263c71e660f997b04ef73
Isabek Tashiev <isabek2309@gmail.com>
2020-07-17 22:53:39 +0300

    Rename TokenService into SharedPreferencesService.
    - It should store info about user.
    - Add remove current user info method.

--- mobile/lib/services/SharedPreferencesService.dart
+++ mobile/lib/services/SharedPreferencesService.dart
@@ -6,16 +6,19 @@ abstract class SharedPreferencesService {
   saveAuthenticationResponse(AuthenticationResponse authenticationResponse);
 
   Future<AuthenticationResponse> getCurrentUserInfo();
+
+  removeCurrentUserInfo();
 }
 
 class TirekSharedPreferencesService implements SharedPreferencesService {
   @override
-  saveAuthenticationResponse(AuthenticationResponse authenticationResponse) async {
+  saveAuthenticationResponse(
+      AuthenticationResponse authenticationResponse) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
     await prefs.setString('token', authenticationResponse.token);
     await prefs.setInt('userId', authenticationResponse.user.id);
-    await prefs.setString('fullName', authenticationResponse.user.fullName);
+    await prefs.setString('userFullName', authenticationResponse.user.fullName);
   }
 
   @override
@@ -23,8 +26,16 @@ class TirekSharedPreferencesService implements SharedPreferencesService {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('token');
     final userId = prefs.getInt('userId');
-    final fullName = prefs.getString('fullName');
+    final fullName = prefs.getString('userFullName');
 
     return AuthenticationResponse(token, User(userId, fullName));
+  }
+
+  @override
+  removeCurrentUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+    await prefs.remove('userId');
+    await prefs.remove('userFullName');
   }
 }
