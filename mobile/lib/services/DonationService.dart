212a78b93cc38ff14fcf5bbd549057510011e33c
Isabek Tashiev <isabek2309@gmail.com>
2020-07-19 14:01:39 +0300

    Add fetching hospitals, donations.

---
+++ mobile/lib/services/DonationService.dart
@@ -0,0 +1,20 @@
+import 'package:tirek_mobile/helper/ApiHelper.dart';
+import 'package:tirek_mobile/models/response/DonataionResponse.dart';
+
+abstract class DonationService {
+  Future<DonationResponse> get();
+}
+
+class TirekDonationService extends DonationService {
+  @override
+  Future<DonationResponse> get() async {
+    final Map<String, String> headers = {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Accept': 'application/json',
+    };
+
+    final responseJson = await ApiHelper.get("donations/", headers);
+
+    return DonationResponse.fromJson(responseJson);
+  }
+}
